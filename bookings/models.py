e08e1b21e8cd060d837ac599f818993c47550b89
Gustaf Starhög <gustafstar@hotmail.com>
2023-07-14 11:16:43 +0000

    added maxvaluevalidator and migrated

--- bookings/models.py
+++ bookings/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
+from django.core.validators import MaxValueValidator
 
 
 BOOKING_STATUS = ((0, "Awaiting Approval"), (1, "Confirmed"))
