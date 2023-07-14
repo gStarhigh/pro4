e2028aaa9b55b1e6fd2d84eff91b7e40e3af6ee6
Gustaf Starhög <gustafstar@hotmail.com>
2023-07-14 12:23:39 +0000

    added test base, index, views and urls to make sure all is connected

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+class Home(generic.TemplateView):
+    """ This view is used to display the home page """
+    template_name = "index.html"
--- equestrian/urls.py
+++ equestrian/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from bookings import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
+    path('', views.Home.as_view(), name='Home'),
 ]
