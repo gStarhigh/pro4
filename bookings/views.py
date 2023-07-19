7f40ae858f7fd6f3f9dfc5b19dd336a83a18833a
Gustaf Starhög <gustafstar@hotmail.com>
2023-07-19 08:44:53 +0000

    created account and login page

--- bookings/views.py
+++ bookings/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-from django.views import generic
+from django.views import generic, View
 
 
 class Home(generic.TemplateView):
     """ This view is used to display the home page """
     template_name = "index.html"
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
+
+class CreateAccountView(View):
+    def get(self, request):
+        return render(request, 'create_account.html')
