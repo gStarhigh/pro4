25177e8ed87b4cc5bc75e83a5ea75e106620f84c
Gustaf Starhög <gustafstar@hotmail.com>
2023-07-19 15:18:59 +0000

    changed debug to false for heroku testing

--- equestrian/settings.py
+++ equestrian/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-gstarhigh-pro4-4wvtoqh6q6l.ws-eu101.gitpod.io',
                  'star-pro4-3dc81848625c.herokuapp.com']
