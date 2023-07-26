26af4199814712dc30128b5cd4b6b24c058dbc9a
Gustaf Starhög <gustafstar@hotmail.com>
2023-07-26 13:41:12 +0000

    added validation for deletion of bookings

--- bookings/validation.py
+++ bookings/validation.py
@@ -22,3 +22,15 @@ def validate_booking_time(value):
     allowed_times = [time(18, 0), time(19, 0), time(20, 0), time(21, 0)]
     if value not in allowed_times:
         raise ValidationError("Invalid booking time")
+
+
+def validate_booking_deletion(booking):
+    """
+    Checks if the booking can be deleted. It should not be able to be
+    deleted within 24 hours of the lesson starting.
+    """
+    time_until_lesson = booking.lesson_datetime - timezone.now()
+    if time_until_lesson.total_seconds() < 24 * 3600:
+        raise ValidationError("You cannot delete a booking within 24 hours"
+                              "of the lessons start. Contact the teacher to "
+                              "find a solution together.")
