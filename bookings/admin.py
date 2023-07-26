b6cc346363125e6181530f832c74e8f323c98391
Gustaf Starhög <gustafstar@hotmail.com>
2023-07-26 09:22:39 +0000

    added more display info to the admin panel

--- bookings/admin.py
+++ bookings/admin.py
@@ -6,10 +6,10 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(LessonBooking)
 class LessonAdmin(SummernoteModelAdmin):
     summernote_fields = ('focus_lesson')
-    list_display = ('booking_id', 'user', 'lesson_date', 'created_on',
-                    'booking_status')
-    search_fields = ('user', 'lesson_date', 'booking_status')
-    list_filter = ('user', 'lesson_date', 'booking_status')
+    list_display = ('booking_id', 'user', 'lesson_date', 'lesson_time',
+                    'no_participants', 'created_on', 'booking_status')
+    search_fields = ('user', 'lesson_date', 'lesson_time', 'booking_status')
+    list_filter = ('user', 'lesson_date', 'lesson_time', 'booking_status')
     actions = ['approve_lesson']
 
     def approve_lesson(self, request, queryset):
