3a4f27df246f79c83531eb9672534f871b07e60f
Gustaf Starhög <gustafstar@hotmail.com>
2023-08-02 12:58:08 +0000

    added a const queryselector for focus_lesson, now it works

--- static/js/sendEmail.js
+++ static/js/sendEmail.js
@@ -1,12 +1,13 @@
 function sendMail(LessonBookingForm) {
     const username = document.querySelector('p[data-username]').getAttribute('data-username');
     const email = document.querySelector('p[data-email]').getAttribute('data-email');
+    const focusLesson = document.querySelector('[name="focus_lesson"]').value;
     const serviceID = "service_bp6z8w3";
     const templateID = "booking_created";
     const templateParams = {
         "from_name": username,
         "from_email": email,
-        "booking_info": "",
+        "booking_info": focusLesson,
 
     }
 
