44404881d245e9ca869de6bd37d965b71a620542
Gustaf Starhög <gustafstar@hotmail.com>
2023-08-02 13:13:07 +0000

    added the remaining info to the emailjs function

--- static/js/sendEmail.js
+++ static/js/sendEmail.js
@@ -1,14 +1,19 @@
 function sendMail(LessonBookingForm) {
     const username = document.querySelector('p[data-username]').getAttribute('data-username');
     const email = document.querySelector('p[data-email]').getAttribute('data-email');
-    const focusLesson = document.querySelector('[name="focus_lesson"]').value;
+    const lessonDate = document.querySelector('[name="lesson_date"]').value;
+    const lessonTime = document.querySelector('[name="lesson_time"]').value;
+    const noParticipants = document.querySelector('[name="no_participants"]').value;
+    const focus = document.querySelector('[name="focus_lesson"]').value;
     const serviceID = "service_bp6z8w3";
     const templateID = "booking_created";
     const templateParams = {
         "from_name": username,
         "from_email": email,
-        "booking_info": focusLesson,
-
+        "lesson_date": lessonDate,
+        "lesson_time": lessonTime,
+        "no_participants": noParticipants,
+        "focus_lesson": focus,
     }
 
     emailjs.send(serviceID, templateID, templateParams)
